6c9ecc8e27b5a00add863b568f186b122898da17
FOURNIER Emilien <emilien.fournier@ensta-bretagne.org>
2019-12-17 16:05:48 +0100

    m4 script for model generation : update

--- src/simple_soup/models/32bits/ss_model.c
+++ src/simple_soup/models/32bits/ss_model.c
@@ -53,10 +53,42 @@ int aA_false_1(ss_state *source) {
     return 0;
 }
 
+//guard action for bit 2
+char gA_true_2(ss_state *source) {
+    return ((source->data>>2) & 1) > 0 && ((TableBits != 0) ? ((TableBits[source->data % BITS_SIZE]>>(2) & 1) > 0) : 1);
+}
+int aA_true_2(ss_state *source) {
+    source->data &= ~ (1 << 2);
+    return 0;
+}
+char gA_false_2(ss_state *source) {
+    return ((source->data>>2) & 1) == 0 && ((TableBits != 0) ? ((TableBits[source->data % BITS_SIZE]>>(2) & 1) > 0) : 1);
+}
+int aA_false_2(ss_state *source) {
+    source->data |= (1 << 2);
+    return 0;
+}
+
+//guard action for bit 3
+char gA_true_3(ss_state *source) {
+    return ((source->data>>3) & 1) > 0 && ((TableBits != 0) ? ((TableBits[source->data % BITS_SIZE]>>(3) & 1) > 0) : 1);
+}
+int aA_true_3(ss_state *source) {
+    source->data &= ~ (1 << 3);
+    return 0;
+}
+char gA_false_3(ss_state *source) {
+    return ((source->data>>3) & 1) == 0 && ((TableBits != 0) ? ((TableBits[source->data % BITS_SIZE]>>(3) & 1) > 0) : 1);
+}
+int aA_false_3(ss_state *source) {
+    source->data |= (1 << 3);
+    return 0;
+}
+
 
 
 ss_soup * ss_make_soup() {
-    ss_behavior **behaviors = (ss_behavior **)malloc(4 * sizeof(ss_behavior *));
+    ss_behavior **behaviors = (ss_behavior **)malloc(8 * sizeof(ss_behavior *));
     
     ss_new_behavior(gA_true_0, aA_true_0, &behaviors[0]);
     ss_new_behavior(gA_false_0, aA_false_0, &behaviors[1]);
@@ -64,12 +96,18 @@ ss_soup * ss_make_soup() {
     ss_new_behavior(gA_true_1, aA_true_1, &behaviors[2]);
     ss_new_behavior(gA_false_1, aA_false_1, &behaviors[3]);
 
+    ss_new_behavior(gA_true_2, aA_true_2, &behaviors[4]);
+    ss_new_behavior(gA_false_2, aA_false_2, &behaviors[5]);
+
+    ss_new_behavior(gA_true_3, aA_true_3, &behaviors[6]);
+    ss_new_behavior(gA_false_3, aA_false_3, &behaviors[7]);
+
 
     ss_state *initial = (ss_state*) malloc(sizeof(ss_state));
     initial->data = 0;
     
     ss_soup *soup = NULL;
-    ss_new_soup(initial, behaviors, 4, &soup);
+    ss_new_soup(initial, behaviors, 8, &soup);
 
     free(behaviors);
     return soup;
